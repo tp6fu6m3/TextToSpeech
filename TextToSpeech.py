b7126c0a05b78e3ea1861af96ff5c133e7a0a5f9
tp6fu6m3 <m3tp6fu6@gmail.com>
2022-10-15 08:09:05 +0800

    Add files via upload

--- TextToSpeech.py
+++ TextToSpeech.py
@@ -20,12 +20,25 @@ class ThreadedCamera(object):
         self.FPS_MS = int(self.FPS * 1000)
 
         # Start frame retrieval thread
-        self.thread = Thread(target=self.update, args=())
-        self.thread.daemon = True
-        self.thread.start()
+        self.tupdate = Thread(target=self.update)
+        self.tupdate.daemon = True
+        self.tshow_frame = Thread(target=self.show_frame)
+        self.tshow_frame.daemon = True
+        #self.tframe2audio = Thread(target=self.frame2audio)
+        #self.tframe2audio.daemon = True
         
-        self.engine = pyttsx3.init()
-
+        self.stop = False
+        #self.engine = pyttsx3.init()
+    
+    def forward(self):
+        self.tupdate.start()
+        time.sleep(1)
+        self.tshow_frame.start()
+        time.sleep(1)
+        #self.tframe2audio.start()
+        #time.sleep(1)
+        
+    
     def update(self):
         while True:
             if self.capture.isOpened():
@@ -33,27 +46,36 @@ class ThreadedCamera(object):
             time.sleep(self.FPS)
 
     def show_frame(self):
-        cv2.imshow('frame', self.frame)
-        cv2.waitKey(self.FPS_MS)
-        
+        while True:
+            try:
+                cv2.imshow('frame', self.frame)
+                if cv2.waitKey(self.FPS_MS) & 0xFF == ord('q'):
+                    cv2.destroyAllWindows()
+                    self.stop = True
+            except:
+                print("An show_frame exception occurred")
+
     def frame2audio(self):
-        img = Image.fromarray(self.frame)
-        text = pytesseract.image_to_string(img, lang="chi_tra+eng")
-        print(text)
-        self.engine.say(text)
-        self.engine.runAndWait()
+        while True:
+            img = Image.fromarray(self.frame)
+            text = pytesseract.image_to_string(img, lang="chi_tra+eng")
+            print(text)
+            self.engine.say(text)
+            self.engine.runAndWait()
+
 
 
 if __name__ == '__main__':
+    engine = pyttsx3.init()
     capture = ThreadedCamera()
+    capture.forward()
     while True:
-        try:
-            capture.show_frame()
-            capture.frame2audio()
-            
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                cv2.destroyAllWindows()
-                break
-        except AttributeError:
-            pass
-
+        img = Image.fromarray(capture.frame)
+        text = pytesseract.image_to_string(img, lang="chi_tra+eng")
+        print(text)
+        engine.say(text)
+        engine.runAndWait()
+        if capture.stop:
+            break
+    
+    
